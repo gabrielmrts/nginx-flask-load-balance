b99f220db87e30df7b4288c84a864013e7984f5e
Gabriel <44853600+gabrielmrts@users.noreply.github.com>
2022-03-06 23:25:21 -0300

    Update app.py

--- app2/app.py
+++ app2/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'APP2'
+    return 'This is the second application'
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
