3728afb85b846d8b575405e7d4207172fbfc028b
Gabriel <44853600+gabrielmrts@users.noreply.github.com>
2022-03-06 23:25:01 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'APP1'
+    return 'This is the first application'
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
